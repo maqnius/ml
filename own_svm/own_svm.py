e215f701674c118e52755d91c4358fb9a177a1d5
Mark Niehues <niehues.mark@gmail.com>
2017-07-10 17:07:07 +0200

    added comments

--- own_svm/own_svm.py
+++ own_svm/own_svm.py
@@ -1,12 +1,29 @@
+#   Copyright (C) 2017 Mark Niehues, Stefaan Hessmann
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#
+
+
 import numpy as np
 import pandas as pd
 from random import randint
 from kernels import kernel_lin, kernel_rbf
 
 class own_smo:
-
-    def __init__(self, k, C):
-        self.k = k
+    """
+    Going to be a more advanced method of choosing the alphas
+    """
+    def __init__(self, C):
         self.C = C
 
     def fit(self, X_train, y_train):
@@ -16,7 +33,11 @@ class own_smo:
         return None
 
 class own_smo_simple:
+    """
+    Implementation of a simple smo algorithm that chooses the comparable pairs randomly
 
+    Main Source: http://cs229.stanford.edu/materials/smo.pdf
+    """
     def __init__(self, C):
         self.C = C
         # self.kernel = kernel_rbf
@@ -28,6 +49,21 @@ class own_smo_simple:
         self.b = 0.
 
     def fit(self, X_train, y_train, max_passes=10, tol=1e-4):
+        """
+        Fits alpha values and the threshold b with given Training data
+
+        Parameters
+        ----------
+        X_train: Numpy Array or Pandas Data Set
+            Training Data Set
+        y_train: Numpy Array oder Pandas Series
+            Labels for Training
+        max_passes: int
+            Maximal Number of runs without any change in the alpha values that
+            determines the end of fitting
+        tol: float
+            Tolerance on estimated Error
+        """
         # Convert arguments to numpy arrays if they are in pandas datastructures
         if type(X_train) == pd.DataFrame:
             self.X_train = X_train.as_matrix()
@@ -38,42 +74,51 @@ class own_smo_simple:
         self.n_test_samples = len(y_train)
         self.alpha = np.zeros(self.n_test_samples)
 
-        passes = 0 # Counting runs without changing a value
+        passes = 0  # Counting runs without changing a value
         while passes < max_passes:
             changed_alpha = False
 
             for i in range(self.n_test_samples):
-                E_i = self.dec_func(i) - self.y_train[i]
+                # Calculate the error with the current alpha
+                E_i = self.dec_func(self.X_train[i]) - self.y_train[i]
 
+                # If accuracy is not satisfying yet
                 if (self.y_train[i] * E_i < -tol and self.alpha[i] < self.C) or \
                         (self.y_train[i] * E_i > tol and self.alpha[i] > 0):
 
+                    # Randomly choose another alpha to pair
                     j = randint(0, self.n_test_samples - 1)
 
                     # Saving old alphas
                     a_i_old = self.alpha[i]
                     a_j_old = self.alpha[j]
 
-                    E_j = self.dec_func(j) - self.y_train[j]
-                    L, H = self.calc_bonds(i, j)
+                    # Calculate the error of the other alpha
+                    E_j = self.dec_func(self.X_train[j]) - self.y_train[j]
 
+                    # Calculate the valid limits that are a consequence of the linear dependence
+                    L, H = self.calc_limits(i, j)
                     if L == H:
                         continue
 
+                    # Evaluate the second derivative of the Loss function for optimizing
+                    # Eta should be negative to make shore, what we are approaching a maximum
                     eta = 2 * self.kernel_ind(i, j) - self.kernel_ind(i, i) - self.kernel_ind(j, j)
                     if eta >= 0:
                         continue
 
                     a_j = a_j_old - self.y_train[j] * (E_i - E_j) / eta
 
+                    # Clip the new alpha to the limits
                     if a_j > H:
                         a_j = H
                     elif a_j < L:
                         a_j = L
 
-                    if np.abs(a_j - a_j_old) < tol:  # Lets check it
+                    if np.abs(a_j - a_j_old) < tol:
                         continue
 
+                    # Calculate the new value for a_i from the new value of a_j
                     a_i = a_i_old + self.y_train[i] * self.y_train[j] * (a_j_old - a_j)
 
                     # Calculate new threshold
@@ -89,13 +134,19 @@ class own_smo_simple:
                     else:
                         self.b = (b_1 + b_2)/2
 
+                    # Replace the alpha values
                     self.alpha[i] = a_i
                     self.alpha[j] = a_j
                     changed_alpha = True
 
             passes += 1 if changed_alpha else 0
 
-    def calc_bonds(self, i, j):
+    def calc_limits(self, i, j):
+        """
+        Calculates the limits for the new alpha value that follow from
+        the linear dependence of two terms
+
+        """
         if self.y_train[i] != self.y_train[j]:
             L = max(0, self.alpha[j] - self.alpha[i])
             H = min(self.C, self.C + self.alpha[j] - self.alpha[i])
@@ -105,24 +156,42 @@ class own_smo_simple:
 
         return L, H
 
-    def dec_func(self, x_ind):
+    def dec_func(self, x):
+        """ Decision function without the signum
+        """
         sum = self.b
         for i in range(self.n_test_samples):
-            sum += self.alpha[i] * self.y_train[i] * self.kernel_ind(i, x_ind)
+            sum += self.alpha[i] * self.y_train[i] * self.kernel(self.X_train[i], x)
 
         return sum
 
     def predict(self, X):
+        """
+        Predicts the Class for an X
+        Parameters
+        ----------
+        X: Array or Float
+            Points whose class shall be predicted
+
+        Returns
+        -------
+        Integer Array or Integer
+            Predicted classes
+        """
         # Convert to numpy arrays
         if type(X) == pd.DataFrame or type(X) == pd.Series:
             X = X.as_matrix()
 
+        # Initialize
         y = np.zeros(X.shape[0])
         for i in range(len(y)):
-            y[i] = self.b
-            for j in range(self.n_test_samples):
-                y[i] += self.alpha[j]*self.y_train[j]*self.kernel(self.X_train[j], X[i])
+            # Apply the decision function to each point
+            y[i] = self.dec_func(X[i])
         return np.sign(y).astype(int)
 
     def kernel_ind(self, i, j):
+        """
+        Wrapper that makes it possible to call the kernel only with the indices
+        of the train set.
+        """
         return self.kernel(self.X_train[i], self.X_train[j])
