79aa400a36aa68a3fb1f15891ce6c4cdfaa18dab
Mark Niehues <niehues.mark@gmail.com>
2017-07-10 11:46:18 +0200

    bisschen was

--- own_svm/own_svm.py
+++ own_svm/own_svm.py
@@ -10,7 +10,7 @@ def svm_test(smo):
     # create some data sets from
     # http://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html.
 
-    X, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
+    X, y = make_classification(n_samples=20, n_features=2, n_redundant=0, n_informative=2,
                                random_state=1, n_clusters_per_class=1)
 
     rng = np.random.RandomState(2)
@@ -28,9 +28,10 @@ def svm_test(smo):
 
     num_test = 0.2 # Part that is used for testing
     X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=num_test, random_state=23)
-
+    print(X_train.shape)
+    exit(0)
     # Create test
-    clf = smo(k = 0.001, C = 100.0)
+    clf = smo(C = 100.0)
     clf.fit(X_train, y_train)
 
     predictions = clf.predict(X_test)
@@ -59,12 +60,98 @@ class own_smo:
 
 class own_smo_simple:
 
-    def __init__(self, k, C):
-        self.k = k
+    def __init__(self, C):
         self.C = C
+        self.X_train = None
+        self.y_train = None
+        self.alpha = None
+        self.b = 0.
 
-    def fit(self, X_train, y_train):
-        return None
+    def fit(self, X_train, y_train, max_passes=10, tol=1e-3):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.alpha = np.zeros(len(y_train))
+
+        passes = 0
+        while passes < max_passes:
+            num_changed_alphas = 0
+
+            for i in range(len(self.y_train)):
+                E_i = self.dec_func(i) - self.y_train[i]
+                if ( self.y_train[i] * E_i < -tol and self.alpha[i] < C ) or \
+                    ( self.y_train[i] * E_i > tol and self.alpha[i] > 0):
+
+                    j = int(np.random.rand()*len(y_train))
+                    # Saving old alphas
+                    a_i_old = self.alpha[i]
+                    a_j_old = self.alpha[j]
+
+                    E_j = self.dec_func(j) - self.y_train[j]
+                    L = self.calc_L(i, j)
+                    H = self.calc_H(i, j)
+
+                    if L == H:
+                        continue
+
+                    eta = 2 * self.kernel(i, j) - self.kernel(i, i) - self.kernel(j, j)
+                    if eta >= 0:
+                        continue
+
+                    a_j = a_j_old - self.y_train[j] * (E_i - E_j) / eta
+
+                    if a_j > H:
+                        a_j = H
+                    elif a_j < L:
+                        a_j = L
+
+                    if np.abs(a_j - a_j_old) < tol: # Lets check it
+                        continue
+
+                    a_i = a_i_old + self.y_train[i] * self.y_train[j] * (a_j_old - a_j)
+
+                    # Calculate new threshold
+                    b_1 = self.b - E_i - self.y_train[i] * (a_i - a_i_old) * self.kernel(i, i) - \
+                        self.y_train[j] * (a_j - a_j_old) * self.kernel(i, j)
+
+                    b_2 = self.b - E_j - self.y_train[i] * (a_i - a_i_old) * self.kernel(i, j) - \
+                        self.y_train[j] * (a_j - a_j_old) * self.kernel(j, j)
+
+                    if C > a_i > 0.:
+                        self.b = b_1
+                    elif C > a_j > 0:
+                        self.b = b_2
+                    else:
+                        self.b = (b_1 + b_2)/2
+
+                    num_changed_alphas += 1
+
+            if num_changed_alphas == 0:
+                passes += 1
+            else:
+                passes = 0
+
+    def calc_L(self, i, j):
+        if self.y_train[i] != self.y_train[j]:
+            return max(0, self.alpha[j] - self.alpha[i])
+        else:
+            return max(0, self.alpha[i] + self.alpha[j] - self.C)
+
+    def calc_H(self, i, j):
+        if self.y_train[i] != self.y_train[j]:
+            return min(self.C, self.C + self.alpha[j] - self.alpha[i])
+        else:
+            return min(self.C, self.alpha[i] + self.alpha[j])
+
+
+    def dec_func(self, x_ind):
+        sum = self.b
+        for i in range(len(self.y_train)):
+            sum += self.alpha[i] * self.y_train[i] * self.kernel(i, x_ind)
+
+        return sum
+
+    def kernel(self, x, y):
+        return self.X_train[x].dot(self.X_train[y])
 
     def predict(self, X):
         return None
